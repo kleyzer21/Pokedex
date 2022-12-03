32ef71be937df9a8963ecb3455188c92f993762c
Edimar Kleyzer <edimar.silva2@estudante.ifms.edu.br>
2022-12-02 22:48:08 -0400

    pokedex

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,30 @@
 import '../css/style.css'
 
+/*Selecionar os elementos no dom*/
+const inputPesquisa = document.querySelector("#inputPesquisa")
+const btnLocalizar = document.querySelector("btnlocalizar")
+const pokedexDisplay = document.querySelector("Display")
 
+/*Adicionando evento */
+btnLocalizar.addEventListener('click', function(){
+    const dadosPokemon = await localizarPokemon(inputPesquisa,value)
+    criarCartao(dadosPokemon)
 
+    /*buscar os dados do pokemon no api */
+    
+})
+async function localizarPokemon(termoBusca){
+const url =`https://pokeapi.co/api/v2/pokemon/${termoBusca}`
+const response = await fetch(url)
+const pokemon = await response.json()
+return pokemon 
+}
+function criarCartao(pokemon){
+    const cartaoPokemon = document.createElement('div')
+    cartaoPokemon.className ='cartaoPokemon'
+    cartaoPokemon.innerHTML=`
+    <img class="pokemonSprite" src="${pokemon.sprite.front_default}"/>
+    <h2>${pokemon.name} </h2>
+    `
+    pokedexDisplay.appendChild(cartaoPokemon)
+}
